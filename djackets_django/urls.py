613dff84af0e2e100340fbf1e4532d4241f33e78
YFarias <yurisfarias@gmail.com>
2022-02-16 09:45:05 +0000

    route, serealizer, models

--- djackets_django/urls.py
+++ djackets_django/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 path('api/v1/', include('djoser.urls')),
 path('api/v1/', include('djoser.urls.authtoken')),
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -13,4 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
-]
+    path('api/v1/', include('product.urls')),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
